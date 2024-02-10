b0813305fbe652d4ea98bba7f20f4ea51ba51da5
vaniallla <vgao825@gmail.com>
2024-02-10 17:30:20 -0500

    created calculator class with inputs

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,1 +1,14 @@
-
+class Calculator:
+  validOperators = ["+", "-", "*", "/"]
+  try:
+    print ("Please enter the first number of your equation")
+    num1 = float(input())
+    print ("Please enter the operator of your equation.")
+    operator = str(input())
+    print ("Please enter the second number of your equation")
+    num2 = float(input())
+  except: 
+    print ("Invalid input, please try again.")
+  else:
+    if operator not in validOperators: raise Exception("Invalid operator inputted. Valid operators are +, -, *, /")
+  print (num1, operator, num2)
